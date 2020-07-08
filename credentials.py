6c8ae93ca2647bec00a030601c73f54640e1c907
dauz1 <58050490+dauz1@users.noreply.github.com>
2020-07-07 23:23:00 -0400

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 14:30:48 2020
+
+@author: zachd
+"""
+
+# Twitter App access keys for @user
+
+# Consume:
+CONSUMER_KEY    = 'sCd2gujFmCLUK6CuN3AtCfNJT'
+CONSUMER_SECRET = 'LJ2EUUgGRY19oCtxMNDTs9DtveYiOwb8quVbKSkFdICXt3MB0p'
+
+# Access:
+ACCESS_TOKEN  = '1244323243479183366-xFATGA1bTYtwVMFaHetemAEWX9Jqw2'
+ACCESS_SECRET = 'qOSOB3UEqFG9tgFVqiShJF5ic4k9jYp4o4blXbluUIvMk'
